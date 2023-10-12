b1d7622bd1d08ac6e7c375a7db2b095419c27eb7
Ratko Simidzija <73665187+Ratko-Sim@users.noreply.github.com>
2023-10-12 22:50:32 +0300

    sort and fix filtering bugs

--- app/issues/(page)/IssueStatusFilter.tsx
+++ app/issues/(page)/IssueStatusFilter.tsx
@@ -2,33 +2,27 @@
 
 import { Status } from '@prisma/client';
 import { Select } from '@radix-ui/themes';
-import { useRouter } from 'next/navigation';
-
-const statuses: { label: string; value?: Status }[] = [
-  {
-    label: 'All',
-  },
-  {
-    label: 'Open',
-    value: 'OPEN',
-  },
-  {
-    label: 'In Progress',
-    value: 'IN_PROGRESS',
-  },
-  {
-    label: 'Closed',
-    value: 'CLOSED',
-  },
-];
+import { useRouter, useSearchParams } from 'next/navigation';
 
 export default function IssueStatusFilter() {
   const router = useRouter();
+  const searchParams = useSearchParams();
 
   return (
     <Select.Root
+      defaultValue={searchParams.get('status') || ''}
       onValueChange={(status) => {
-        const query = status ? `?status=${status}` : '';
+        const params = new URLSearchParams();
+
+        if (status) {
+          params.append('status', status);
+        }
+
+        if (searchParams.get('orderBy')) {
+          params.append('orderBy', searchParams.get('orderBy')!);
+        }
+
+        const query = params.size ? '?' + params.toString() : '';
         router.push('/issues' + query);
       }}
     >
@@ -48,3 +42,21 @@ export default function IssueStatusFilter() {
     </Select.Root>
   );
 }
+
+const statuses: { label: string; value?: Status }[] = [
+  {
+    label: 'All',
+  },
+  {
+    label: 'Open',
+    value: 'OPEN',
+  },
+  {
+    label: 'In Progress',
+    value: 'IN_PROGRESS',
+  },
+  {
+    label: 'Closed',
+    value: 'CLOSED',
+  },
+];
--- app/issues/(page)/page.tsx
+++ app/issues/(page)/page.tsx
@@ -1,26 +1,54 @@
 import { IssueStatusBadge, Link } from '@/app/components/index';
 import prisma from '@/prisma/client';
-import { Status } from '@prisma/client';
+import { Issue, Status } from '@prisma/client';
 import { Table } from '@radix-ui/themes';
+import NextLink from 'next/link';
 import IssueActions from './IssueActions';
+
+type Column = {
+  label: string;
+  value: keyof Issue;
+  className?: string;
+};
 
 export default async function IssuesPage({
   searchParams,
 }: {
-  searchParams: { status: Status };
+  searchParams: { status: Status; orderBy: keyof Issue };
 }) {
+  const columns: Column[] = [
+    {
+      label: 'Issue',
+      value: 'title',
+    },
+    {
+      label: 'Status',
+      value: 'status',
+      className: 'hidden md:table-cell',
+    },
+    {
+      label: 'Created',
+      value: 'createdAt',
+      className: 'hidden md:table-cell',
+    },
+  ];
+
   const statuses = Object.values(Status);
   const status = statuses.includes(searchParams.status)
     ? searchParams.status
+    : undefined;
+
+  const orderBy = columns
+    .map((column) => column.value)
+    .includes(searchParams.orderBy)
+    ? { [searchParams.orderBy]: 'asc' }
     : undefined;
 
   const issues = await prisma.issue.findMany({
     where: {
       status: status,
     },
-    orderBy: {
-      createdAt: 'desc',
-    },
+    orderBy,
   });
 
   return (
@@ -29,13 +57,23 @@ export default async function IssuesPage({
       <Table.Root variant='surface'>
         <Table.Header>
           <Table.Row>
-            <Table.ColumnHeaderCell>Issue</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell className='hidden md:table-cell'>
-              Status
-            </Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell className='hidden md:table-cell'>
-              Created
-            </Table.ColumnHeaderCell>
+            {columns.map((column: Column) => (
+              <Table.ColumnHeaderCell
+                key={column.value}
+                className={column.className}
+              >
+                <NextLink
+                  href={{
+                    query: { ...searchParams, orderBy: column.value },
+                  }}
+                >
+                  {column.label}
+                </NextLink>
+                {column.value === searchParams.orderBy && (
+                  <span className='ml-1'>&#8595;</span>
+                )}
+              </Table.ColumnHeaderCell>
+            ))}
           </Table.Row>
         </Table.Header>
         <Table.Body>
@@ -43,7 +81,7 @@ export default async function IssuesPage({
             <Table.Row key={issue.id}>
               <Table.RowHeaderCell>
                 <Link href={`/issues/${issue.id}`}>{issue.title}</Link>
-                <div className='block md:hidden'>
+                <div className='mt-2 block md:hidden'>
                   <IssueStatusBadge status={issue.status} />
                 </div>
               </Table.RowHeaderCell>
